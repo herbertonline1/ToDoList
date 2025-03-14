2ba4b8587465738d365f99ab494a2b2458f99fca
Herbert Ramos <79092747+herbertonline1@users.noreply.github.com>
2025-03-14 18:09:58 -0300

    Update todolist.js

--- src/todolist.js
+++ src/todolist.js
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from 'react'; // Importa React e hooks useState e useEffect
-import './App.css'; // Importa o arquivo de estilos CSS
+import React, { useState, useEffect } from 'react'; 
+import './App.css'; 
 
 function Todolist() {
-  const [tasks, setTasks] = useState([]); //  armazenar a lista de tarefas
-  const [taskName, setTaskName] = useState(''); //  armazenar o nome da tarefa
-  const [taskDescription, setTaskDescription] = useState(''); // armazenar a descrição da tarefa
-  const [deletedCount, setDeletedCount] = useState(0); //  contar o número de tarefas deletadas
-  const [joke, setJoke] = useState(''); // Armazenando a piada do Chuck Norris
+  const [tasks, setTasks] = useState([]); 
+  const [taskName, setTaskName] = useState(''); 
+  const [taskDescription, setTaskDescription] = useState(''); 
+  const [deletedCount, setDeletedCount] = useState(0); 
+  const [joke, setJoke] = useState(''); 
 
   useEffect(() => {
-    //  API do Chuck Norris
+   
     const fetchJoke = () => {
-      fetch('https://api.chucknorris.io/jokes/random') // Fazendo a requisição GET para a API
-        .then(response => response.json()) // Converte a resposta para JSON
-        .then(data => setJoke(data.value)); // Define a piada no estado joke
+      fetch('https://api.chucknorris.io/jokes/random') 
+        .then(response => response.json()) 
+        .then(data => setJoke(data.value)); 
     };
 
-    fetchJoke(); // Busca a piada inicialmente
+    fetchJoke(); 
 
-    const intervalId = setInterval(fetchJoke, 5000); // Atualiza a piada a cada 5 segundos
+    const intervalId = setInterval(fetchJoke, 5000); 
 
-    return () => clearInterval(intervalId); // Limpa o intervalo quando o componente é desmontado
-  }, []); // O array vazio [] significa que este efeito só executa uma vez, quando o componente é montado
+    return () => clearInterval(intervalId); 
+  }, []); 
 
   const addTask = () => {
-    // Função para adicionar uma nova tarefa
+   
     if (taskName.trim() && taskDescription.trim()) {
-      // Verifica se o nome e a descrição da tarefa não estão vazios
-      const now = new Date(); // Obtém a data e hora atuais
-      const timestamp = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`; // Formata a data e hora
-      setTasks([...tasks, { name: taskName, description: taskDescription, timestamp }]); // Adiciona a nova tarefa à lista de tarefas
-      setTaskName(''); // Limpa o campo de nome da tarefa
-      setTaskDescription(''); // Limpa o campo de descrição da tarefa
+      
+      const now = new Date(); 
+      const timestamp = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`; 
+      setTasks([...tasks, { name: taskName, description: taskDescription, timestamp }]); 
+      setTaskName(''); 
+      setTaskDescription(''); 
     }
   };
 
   const deleteTask = (index) => {
-    // Função para deletar uma tarefa
-    const newTasks = tasks.filter((_, i) => i !== index); // Filtra a lista de tarefas para remover a tarefa com o índice especificado
-    setTasks(newTasks); // Atualiza a lista de tarefas
-    setDeletedCount(deletedCount + 1); // Incrementa o contador de tarefas deletadas
+    
+    const newTasks = tasks.filter((_, i) => i !== index); 
+    setTasks(newTasks); 
+    setDeletedCount(deletedCount + 1); 
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1><b>To</b>day</h1> {/* Título principal */}
-        <p>Wake up, go ahead, do the thing not tomorrow, do <b>to</b>day</p> {/* Subtítulo */}
+        <h1><b>To</b>day</h1> 
+        <p>Wake up, go ahead, do the thing not tomorrow, do <b>to</b>day</p> 
         <h2>Add new to do</h2> {/* Título da seção de adicionar nova tarefa */}
         <div className="counter-container">
           <p>Finished tasks quantity</p> {/* Título da seção de quantidade de tarefas deletadas */}
@@ -57,20 +57,20 @@ function Todolist() {
           value={taskName}
           onChange={(e) => setTaskName(e.target.value)}
           placeholder="Task Name"
-        /> {/* Campo de entrada para o nome da tarefa */}
+        /> 
         <input
           type="text"
           value={taskDescription}
           onChange={(e) => setTaskDescription(e.target.value)}
           placeholder="Task Description"
-        /> {/* Campo de entrada para a descrição da tarefa */}
+        /> 
         <button onClick={addTask}>Create todo</button> {/* Botão para criar a nova tarefa */}
         <ul>
           {tasks.map((task, index) => (
             <li key={index}>
               <b>{task.name}</b>: {task.description} <br />
               <small>{task.timestamp}</small> <span style={{ color: 'red', cursor: 'pointer' }} onClick={() => deleteTask(index)}>x</span>
-              {/* Exibe o nome, descrição e timestamp da tarefa, e um "x" vermelho para deletar a tarefa */}
+              
             </li>
           ))}
         </ul>
@@ -81,4 +81,4 @@ function Todolist() {
   );
 }
 
-export default Todolist; // Exporta o componente Todolist como padrão
+export default Todolist; 
